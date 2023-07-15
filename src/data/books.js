41ff028c05f5f58f0638ecee2a7147a2d98b8954
Guillermo San Martín <guille.seba0802@gmail.com>
2023-07-14 22:16:52 -0300

    transition view test

---
+++ src/data/books.js
@@ -0,0 +1,34 @@
+export const books = [
+  {
+    id: 'clean-code',
+    title: 'Clean Code: Manual de desarrollo ágil de software',
+    img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1436202607i/3735293.jpg',
+    opinion:
+      'Clean Code is considered a valuable reference for any developer seeking to improve their programming skills and elevate the quality of their code. It helps foster good practices and build more robust and maintainable systems.',
+    author: 'Robert C. Martin',
+  },
+  {
+    id: 'the-clean-coder',
+    title: 'The Clean Coder: A Code of Conduct for Professional Programmers',
+    img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1347470803i/10284614.jpg',
+    opinion:
+      '"The Clean Coder" is considered a valuable resource for developers looking to grow professionally and learn how to become efficient, ethical, and responsible programmers. It offers a realistic view of the profession and provides practical insights to tackle common challenges in software development.',
+    author: 'Robert C. Martin',
+  },
+  {
+    id: 'javascript-the-good-parts',
+    title: 'JavaScript: The Good Parts',
+    img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1328834793i/2998152.jpg',
+    opinion:
+      '"JavaScript: The Good Parts" is considered a valuable guide for JavaScript programmers as it provides a deep understanding of the language\'s fundamental features and helps avoid common pitfalls. It is recommended reading for those who want to write clean, robust, and efficient JavaScript code.',
+    author: 'Douglas Crockford',
+  },
+  {
+    id: 'eloquent-javascript',
+    title: 'Eloquent JavaScript: A Modern Introduction to Programming',
+    img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1308260856i/8910666.jpg',
+    opinion:
+      '"Eloquent JavaScript: A Modern Introduction to Programming" is considered an excellent introduction to programming and JavaScript. It provides a solid foundation for beginners and offers a practical approach that helps readers build programming skills while working on real projects.',
+    author: 'Marijn Haverbeke',
+  },
+];
